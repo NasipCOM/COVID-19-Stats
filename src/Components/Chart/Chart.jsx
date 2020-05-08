f158590d478aff9bee4677ceea58ab919ec4980a
NasipCOM <salmahunov@mail.ru>
2020-05-08 21:37:09 +0600

    Daily data, chartjs

--- src/Components/Chart/Chart.jsx
+++ src/Components/Chart/Chart.jsx
@@ -1,6 +1,31 @@
 import React, { useState, useEffect, } from 'react';
+import { fetchDailyData } from '../../api';
+import { Line, Bar } from 'react-chartjs-2';
 
 const Chart = () => {
+    const [dailyData, setDailyData] = useState({});
+
+    useEffect(() => {
+        const fetchMyAPI = async () => {
+            setDailyData(await fetchDailyData());
+        };
+        console.log(dailyData);
+        fetchMyAPI();
+
+    });
+
+    const lineChart = (
+        dailyData[0]
+            ? (
+                <Line
+                    data={{
+                        labels: '',
+                        datasets: [{}, {}],
+                    }}
+                />
+            ) : null)
+        ;
+
     return (
         <h1></h1>
     )
