7236bb5c593de6e0fb83683721bd7b8c7e6a7c90
NasipCOM <salmahunov@mail.ru>
2020-05-10 03:56:36 +0600

    Updates

---
+++ precache-manifest.6ac7bd41440d549446ef4e61339ef275.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c870b3313188a3426ae3546068154b6",
+    "url": "/COVID-19-Stats/index.html"
+  },
+  {
+    "revision": "660b5f61167443dc3404",
+    "url": "/COVID-19-Stats/static/css/main.397e617d.chunk.css"
+  },
+  {
+    "revision": "6e018dd2db528fb8d0cd",
+    "url": "/COVID-19-Stats/static/js/2.ef3d5f1e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19-Stats/static/js/2.ef3d5f1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "660b5f61167443dc3404",
+    "url": "/COVID-19-Stats/static/js/main.78f753d8.chunk.js"
+  },
+  {
+    "revision": "38c908e8f517f61a3bad",
+    "url": "/COVID-19-Stats/static/js/runtime-main.9d389087.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19-Stats/static/media/image.d7265326.png"
+  }
+]);
