3022cf7b2abc2da2c896455c35a6273c1febd540
NasipCOM <salmahunov@mail.ru>
2020-05-09 12:40:52 +0600

    Responsive design

--- src/api/index.js
+++ src/api/index.js
@@ -1,50 +1,39 @@
 import axios from 'axios';
 
-
 const url = 'https://covid19.mathdro.id/api';
 
 export const fetchData = async (country) => {
-    let changeableUrl = url;
+  let changeableUrl = url;
 
-    if(country){
-        changeableUrl = `${url}/countries${country}`
-    }
-    try {
-        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableUrl)
+  if (country) {
+    changeableUrl = `${url}/countries/${country}`;
+  }
 
-        return { confirmed, recovered, deaths, lastUpdate };
-    }
+  try {
+    const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableUrl);
 
-    catch (error) {
-
-    }
-}
+    return { confirmed, recovered, deaths, lastUpdate };
+  } catch (error) {
+    return error;
+  }
+};
 
 export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`);
 
-    try {
-        const { data } = await axios.get(`${url}/daily`);
-
-        const modifiedData = data.map((dailyData) => ({
-            confirmed: dailyData.confirmed.total,
-            deaths: dailyData.deaths.total,
-            date: dailyData.reportDate,
-        }));
-        return modifiedData
-    }
-
-    catch (error) {
-        console.log(error)
-
-    }
-}
+    return data.map(({ confirmed, deaths, reportDate: date }) => ({ confirmed: confirmed.total, deaths: deaths.total, date }));
+  } catch (error) {
+    return error;
+  }
+};
 
 export const fetchCountries = async () => {
-    try {
-        const { data: { countries } } = await axios.get(`${url}/countries`);
-            return countries.map((country) => country.name)
-    }
-    catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    const { data: { countries } } = await axios.get(`${url}/countries`);
+
+    return countries.map((country) => country.name);
+  } catch (error) {
+    return error;
+  }
+};
